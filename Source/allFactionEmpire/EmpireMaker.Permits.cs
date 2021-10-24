9df157a2636caf773f232390ab3ba6f00b864e64
David Chuang <dc1770714@gmail.com>
2021-10-24 16:08:13 -0400

    Added prerequisite copying, and made permit prerequisites match with Empire permits.

--- Source/allFactionEmpire/EmpireMaker.Permits.cs
+++ Source/allFactionEmpire/EmpireMaker.Permits.cs
@@ -14,11 +14,12 @@ namespace empireMaker
 
         private static bool GenerateRoyalPermits(ConversionParams settings, FactionDef factionDef,
             TechLevel techLevel, List<PawnKindDef> allPawns, List<RoyalTitlePermitDef> baseRoyalPermits,
-            out Dictionary<string, RoyalTitlePermitDef> generatedPermitDefs)
+            out Dictionary<string, RoyalTitlePermitDef> generatedPermitMap)
         {
-            // by the time this is run, factiondef should be one of the valid techlevels.
-            generatedPermitDefs = new Dictionary<string, RoyalTitlePermitDef>();
-            HashSet<RoyalTitlePermitDef> basePermitsUsed = new HashSet<RoyalTitlePermitDef>();
+            // this dictionary contains ORIGINAL defNames as keys, GENERATED defs as values.
+            generatedPermitMap = new Dictionary<string, RoyalTitlePermitDef>();
+
+            HashSet<RoyalTitlePermitDef> basePermits = new HashSet<RoyalTitlePermitDef>();
 
             var p = EmpireHelpers.GetDefNamePrefix(techLevel);
 
@@ -43,18 +44,18 @@ namespace empireMaker
             // add permits to database if they aren't already loaded
             if (militarySmall != null) {
                 DefDatabase<RoyalTitlePermitDef>.Add(militarySmall);
-                generatedPermitDefs.Add(aidSmallDefName, militarySmall);
-                basePermitsUsed.Add(callMilitaryAidSmall);
+                generatedPermitMap.Add(aidSmallDefName, militarySmall);
+                basePermits.Add(callMilitaryAidSmall);
             }
             if (militaryLarge != null) {
                 DefDatabase<RoyalTitlePermitDef>.Add(militaryLarge);
-                generatedPermitDefs.Add(aidLargeDefName, militaryLarge);
-                basePermitsUsed.Add(callMilitaryAidLarge);
+                generatedPermitMap.Add(aidLargeDefName, militaryLarge);
+                basePermits.Add(callMilitaryAidLarge);
             }
             if (militaryGrand != null) {
                 DefDatabase<RoyalTitlePermitDef>.Add(militaryGrand);
-                generatedPermitDefs.Add(aidGrandDefName, militaryGrand);
-                basePermitsUsed.Add(callMilitaryAidGrand);
+                generatedPermitMap.Add(aidGrandDefName, militaryGrand);
+                basePermits.Add(callMilitaryAidGrand);
             }
 
             // Trade permits --
@@ -78,9 +79,9 @@ namespace empireMaker
             DefDatabase<RoyalTitlePermitDef>.Add(tradeCaravan);
             DefDatabase<RoyalTitlePermitDef>.Add(tradeOrbital);
 
-            generatedPermitDefs.Add("TradeSettlement", tradeSettlement);
-            generatedPermitDefs.Add("TradeCaravan", tradeCaravan);
-            generatedPermitDefs.Add("TradeOrbital", tradeOrbital);
+            generatedPermitMap.Add("TradeSettlement", tradeSettlement);
+            generatedPermitMap.Add("TradeCaravan", tradeCaravan);
+            generatedPermitMap.Add("TradeOrbital", tradeOrbital);
 
             // 계급에 따른 거래제한
             if (settings.RequiresTradePermit) {
@@ -122,7 +123,7 @@ namespace empireMaker
             }
 
             foreach (var permit in baseRoyalPermits) {
-                if (basePermitsUsed.Contains(permit)) {
+                if (basePermits.Contains(permit)) {
                     if (debugMode) {
                         Log.Message($"Not cloning {permit.defName}.");
                     }
@@ -134,7 +135,26 @@ namespace empireMaker
                         Log.Message($"Cloning {permit.defName} to {def.defName}. Faction: {def.faction.defName}");
                     }
 
+                    generatedPermitMap.Add(permit.defName, def);
                     DefDatabase<RoyalTitlePermitDef>.Add(def);
+                }
+            }
+
+            foreach (var pair in generatedPermitMap) {
+                RoyalTitlePermitDef def = pair.Value;
+
+                if (def.prerequisite == null) continue;
+                else {
+                    string prerequisiteDefName = def.prerequisite.defName;
+
+                    if (generatedPermitMap.ContainsKey(prerequisiteDefName)) {
+                        var newPermit = generatedPermitMap[prerequisiteDefName];
+
+                        def.prerequisite = newPermit;
+                    }
+                    else {
+                        Log.Error($"F2E royal permit {def.defName} has permit {def.prerequisite.defName} referenced as a prerequisite, but the referenced permit has not been copied to this faction. This should not happen.");
+                    }
                 }
             }
 
@@ -158,7 +178,8 @@ namespace empireMaker
             return def;
         }
 
-        private static Dictionary<EmpireArchetype, List<RoyalTitlePermitDef>> GetBaseRoyalPermits() {
+        private static Dictionary<EmpireArchetype, List<RoyalTitlePermitDef>> GetBaseRoyalPermits()
+        {
             var dict = new Dictionary<EmpireArchetype, List<RoyalTitlePermitDef>>();
 
             foreach (object value in Enum.GetValues(typeof(EmpireArchetype))) {
@@ -169,15 +190,15 @@ namespace empireMaker
                 }
             }
 
-            foreach (var permitDef in from permit 
+            foreach (var permitDef in from permit
                                       in DefDatabase<RoyalTitlePermitDef>.AllDefs
                                       where permit != null
                                             && permit.defName.Length > 4
-                                            && permit.defName.Substring(0,4) == "f2e_"
+                                            && permit.defName.Substring(0, 4) == "f2e_"
                                       select permit) {
                 if (permitDef.faction == null)
                     Log.Error($"F2E permit {permitDef.defName} has no faction - this should not happen.");
-                
+
                 // get rid of f2e_
                 // ex: f2e_IndustrialOutlander => IndustrialOutlander
                 var permitArchetype = permitDef.faction.defName.Substring(4);
